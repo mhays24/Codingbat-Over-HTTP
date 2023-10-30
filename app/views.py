07053ab835d9d8bdb1f26c4351ee07867fe4f563
mhays24 <mhays24@basecampcodingacademy.org>
2023-10-30 13:33:39 -0500

    added type anatations/fix urls

--- app/views.py
+++ app/views.py
@@ -3,20 +3,20 @@ from django.http.response import HttpResponse
 from django.http.request import HttpRequest
 
 
-def number_view(request: HttpRequest, n) -> HttpResponse:
+def number_view(request: HttpRequest, n:int) -> HttpResponse:
     def near_hundred(n):
         return abs(100 - n) <= 10 or abs(200 - n) <= 10
     return HttpResponse(near_hundred(n))
 
 
-def string_splosion_view(request: HttpRequest, word) -> HttpResponse:
+def string_splosion_view(request: HttpRequest, word:str) -> HttpResponse:
     result = ""
     for words in range(len(word)):
         result += word[: words + 1]
     return HttpResponse(result)
 
 
-def cat_dog_view(request: HttpRequest, animal) -> HttpResponse:
+def cat_dog_view(request: HttpRequest, animal:str) -> HttpResponse:
     count_cat = animal.count("cat")
     count_dog = animal.count("dog")
     if count_cat == count_dog:
@@ -25,7 +25,7 @@ def cat_dog_view(request: HttpRequest, animal) -> HttpResponse:
         return HttpResponse(False)
 
 
-def lone_sum_view(request: HttpRequest, a, b, c) -> HttpResponse:
+def lone_sum_view(request: HttpRequest, a:int, b:int, c:int) -> HttpResponse:
     if a == b == c:
         return HttpResponse(0)
     elif a == b:
--- config/urls.py
+++ config/urls.py
@@ -6,9 +6,9 @@ from app.views import number_view, string_splosion_view, cat_dog_view, lone_sum_
 
 
 urlpatterns = [
-    path("number/<int:n>/", number_view),
-    path("string_splosion/<str:word>/", string_splosion_view),
-    path("cat-dog/<str:animal>/", cat_dog_view),
-    path("lone_sum/<int:a>/<int:b>/<int:c>/", lone_sum_view),
+    path("warmup-1/near-hundred/<int:n>/", number_view),
+    path("warmup-2/string-splosion/<str:word>/", string_splosion_view),
+    path("string-2/cat-dog/", cat_dog_view),
+    path("logic-2/lone-sum/<int:a>/<int:b>/<int:c>/", lone_sum_view),
     path("admin/", admin.site.urls),
 ]
